d6077a01df8285ba6adfeb3515f8ada995d6f167
Hitoki <melendrezomar2@gmail.com>
2021-08-16 11:46:29 -0300

    feat filter of clients

--- client/src/pages/Clients/index.js
+++ client/src/pages/Clients/index.js
@@ -7,22 +7,36 @@ import CardClients from "../../components/CardClients";
 const Clients = () => {
 
 	const [clients, setClients] = useState([])
+	const [filterClients, setFilterClients] = useState([])
 
 	useEffect(() => {
 		axios.get("/api/client")
 			.then(res => {
 				setClients(res.data.clients)
+				setFilterClients(res.data.clients)
 			})
 	}, [])
+
+	const handleFilterClients = (e) => {
+		const filtereds = clients.filter(client => {
+			return client.first_name.toLowerCase().includes(e.target.value.toLowerCase()) || client.last_name.toLowerCase().includes(e.target.value.toLowerCase())
+		})
+		setFilterClients(filtereds)
+	}
 
 	return (
 		<div className={styles.container}>
 			<h2 className={styles.title}>Listado de clientes</h2>
+			<input onChange={(e)=> {handleFilterClients(e)}} className={styles.filter} type="text" placeholder="Busque por nombre o apelldio." />
 			<div className={styles.main_container}>
 				{
-					clients.length && clients.map(client => {
+					filterClients.length ? filterClients.map(client => {
 						return <CardClients client={client} />
 					})
+					:
+					(
+						<h3>Cliente no encontrado</h3>
+					)
 				}
 			</div>
 		</div>
